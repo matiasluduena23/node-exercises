585ca5b5897570b2a5f7a20c79e56ae23273ea26
matias ludueña <matiasluduena23@gmail.com>
2023-12-09 20:55:01 -0300

    add router

--- main.js
+++ main.js
@@ -1,48 +1,65 @@
-
-const routeResponseMap = {
-    '/info': '<h1>Info</h1>',
-    '/contact': '<h1>Contact</h1>',
-    '/about': '<h1>About Us</h1>',
-    '/hello': '<h1>Hello</h1>',
-    '/error': '<h1>Error 404 page not fond</h1>',
-  
-}
-
 const port = 3000,
     http = require('http'),
     httpStatus = require('http-status-codes'),
-    app = http.createServer();
+    fs = require('fs');
 
-app.on('request', (req, res) => {
+
+http.createServer( (req, res) => {
+    const url = req.url;
+
+    if(url.indexOf(".html") !== -1){
+        res.writeHead(httpStatus.OK, {
+            "Content-Type": "text/html"
+        });
+        custonReadFile(`./view${url}`, res)
+
+    }else if(url.indexOf(".js") !== -1){
+        res.writeHead(httpStatus.OK, {
+            "Content-Type": "text/javascript"
+        });
+        custonReadFile(`./public/js${url}`, res)
+
+    }else if(url.indexOf(".css") !== -1){
+        res.writeHead(httpStatus.OK, {
+            "Content-Type": "text/css"
+        });
+        custonReadFile(`./public/css${url}`, res)
+
+    }else if(url.indexOf(".png") !== -1){
+        res.writeHead(httpStatus.OK, {
+            "Content-Type": "image/png"
+        });
+        custonReadFile(`./public/images${url}`, res)
+    }
     
-   let body = []
-   req.on('data', (bodyData) => {
-    body.push(bodyData)
-   })
-
-   req.on('end', ()=> {
-    body = Buffer.concat(body).toString();
-    console.log(`Request Body Contents: ${body}`)
-    })
-
-    res.writeHead(httpStatus.OK, {
-        "Content-Type": "text7html"
-    });
-    
-    
-    console.log(req.method)
-    console.log(req.url)
-    console.log(getJsonString(req.headers))
-
-    if(routeResponseMap[req.url]) {
-        res.end(routeResponseMap[req.url])
-    }  else res.end('<h1>Heading</h1>')
-    
-});
-
-app.listen(port)
+}).listen(port)
 console.log('Server listen on port '+ port)
 
-function getJsonString(obj) {
-    return JSON.stringify(obj, null ,2);
+
+function custonReadFile(file_path, res){
+    if(fs.existsSync(file_path)){
+        fs.readFile(file_path, (error, data)=> {
+            if(error){
+                console.log(error)
+                sendErrorResponse(res);
+                return;
+            }
+            res.write(data)
+            res.end()
+        });
+    }else {
+        sendErrorResponse(res);
+    }
+}
+
+const getViewUrl = (url) =>{
+    return `view${url}.html`;
+}
+
+const sendErrorResponse = res => {
+    res.writeHead(httpStatus.NOT_FOUND, {
+        "Content-Type": "text/html"
+    });
+    res.write('<h1>Sorry, not found</h1>')
+    res.end()
 }
